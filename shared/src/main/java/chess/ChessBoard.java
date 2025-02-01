4a6fcb867266a153b8faab52eeafcec063b06011
Patrick Blood <pbloo@Patrick-Blood.com>
2025-02-01 15:45:00 -0700

    implemented clone function

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -9,11 +9,32 @@ import java.util.Objects;
  * Note: You can add to this class, but you may not alter
  * signature of the existing methods.
  */
-public class ChessBoard {
+public class ChessBoard implements Cloneable {
     private final ChessPiece[][] board = new ChessPiece[8][8];
+
     public ChessBoard() {
         
     }
+
+    @Override
+    public ChessBoard clone() {
+        try {
+            ChessBoard clone = (ChessBoard) super.clone();
+
+            for (int i = 0; i < 8; i++) {
+                for (int j = 0; j < 8; j++) {
+                    ChessPiece piece = board[i][j];
+
+                    clone.board[i][j] = new ChessPiece(piece.getTeamColor(), piece.getPieceType());
+                }
+            }
+
+            return clone;
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 
     @Override
     public boolean equals(Object o) {
