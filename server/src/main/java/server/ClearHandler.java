16692219c9e52f96905213e2cab2dfebcba8ba76
Patrick Blood <pbloo@Patrick-Blood.com>
2025-02-20 22:24:44 -0700

    Implemented ClearHandler and added it to the Server class

---
+++ server/src/main/java/server/ClearHandler.java
@@ -0,0 +1,24 @@
+package server;
+
+
+import service.ClearService;
+import spark.Request;
+import spark.Response;
+
+public class ClearHandler {
+    private final ClearService clearService;
+
+    public ClearHandler(ClearService clearService) {
+        this.clearService = clearService;
+    }
+
+    public Object clear(Request req, Response res) {
+        clearService.clear();
+        System.out.println("Database Cleared");
+
+        res.status(200);
+
+        return "{}";
+    }
+
+}
--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,8 +1,32 @@
 package server;
 
+import dataaccess.AuthDAO;
+import dataaccess.GameDAO;
+import dataaccess.UserDAO;
+import dataaccess.memory.MemoryAuthDAO;
+import dataaccess.memory.MemoryGameDAO;
+import dataaccess.memory.MemoryUserDAO;
+import service.ClearService;
 import spark.*;
 
 public class Server {
+
+    private final AuthDAO authDAO;
+    private final GameDAO gameDAO;
+    private final UserDAO userDAO;
+
+    private final ClearService clearService;
+
+    private final ClearHandler clearHandler;
+
+    public Server() {
+        authDAO = new MemoryAuthDAO();
+        gameDAO = new MemoryGameDAO();
+        userDAO = new MemoryUserDAO();
+
+        clearService = new ClearService(gameDAO, userDAO, authDAO);
+        clearHandler = new ClearHandler(clearService);
+    }
 
     public int run(int desiredPort) {
         Spark.port(desiredPort);
@@ -10,12 +34,21 @@ public class Server {
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
+        Spark.delete("/db", clearHandler::clear);
+
 
         //This line initializes the server and can be removed once you have a functioning endpoint 
         Spark.init();
 
         Spark.awaitInitialization();
         return Spark.port();
+    }
+
+    private Object test(Request req, Response res) {
+        System.out.println("Hello World");
+
+        res.status(200);
+        return "{}";
     }
 
     public void stop() {
