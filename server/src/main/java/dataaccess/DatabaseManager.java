d505a22085175f4b099b62c31b8e465bd773585a
Patrick Blood <pbloo@Patrick-Blood.com>
2025-03-06 13:53:03 -0700

    initialized user table

--- server/src/main/java/dataaccess/DatabaseManager.java
+++ server/src/main/java/dataaccess/DatabaseManager.java
@@ -38,7 +38,7 @@ public class DatabaseManager {
     /**
      * Creates the database if it does not already exist.
      */
-    static void createDatabase() throws DataAccessException {
+    public static void createDatabase() throws DataAccessException {
         try {
             var statement = "CREATE DATABASE IF NOT EXISTS " + DATABASE_NAME;
             var conn = DriverManager.getConnection(CONNECTION_URL, USER, PASSWORD);
@@ -62,7 +62,7 @@ public class DatabaseManager {
      * }
      * </code>
      */
-    static Connection getConnection() throws DataAccessException {
+    public static Connection getConnection() throws DataAccessException {
         try {
             var conn = DriverManager.getConnection(CONNECTION_URL, USER, PASSWORD);
             conn.setCatalog(DATABASE_NAME);
--- server/src/main/java/dataaccess/MySQL/MySQLUserDAO.java
+++ server/src/main/java/dataaccess/MySQL/MySQLUserDAO.java
@@ -1,17 +1,27 @@
 package dataaccess.MySQL;
 
+import com.google.gson.Gson;
+import dataaccess.DatabaseManager;
 import dataaccess.UserDAO;
 import dataaccess.exceptions.DataAccessException;
 import models.UserData;
 
+import java.sql.SQLException;
+
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
+
 public class MySQLUserDAO implements UserDAO {
+    public MySQLUserDAO() throws DataAccessException, SQLException {
+        configureDatabase();
+    }
+
     @Override
     public UserData getUser(String username) {
         return null;
     }
 
     @Override
-    public void createUser(UserData user) throws DataAccessException {
+    public void createUser(UserData user) throws DataAccessException{
 
     }
 
@@ -23,6 +33,29 @@ public class MySQLUserDAO implements UserDAO {
     @Override
     public void clear() {
 
+    }
+
+    private final String[] createStatements = {
+            """
+            CREATE TABLE IF NOT EXISTS users (
+            'id' int NOT NULL AUTO_INCREMENT,
+            'username' varchar(255) NOT NULL,
+            'password' varchar(255) NOT NULL,
+            'email' varchar(255) NOT NULL,
+            PRIMARY KEY ('id'),
+            )
+            """
+    };
+
+    private void configureDatabase() throws SQLException, DataAccessException {
+        DatabaseManager.createDatabase();
+        try (var conn = DatabaseManager.getConnection()) {
+            for (var statement : createStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        }
     }
 }
 
