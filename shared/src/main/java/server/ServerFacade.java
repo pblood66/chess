c885525b8dab0c76fb35479a1c96d6688ebba150
Patrick Blood <pbloo@Patrick-Blood.com>
2025-03-17 15:18:44 -0600

    Added ServerFacade

---
+++ shared/src/main/java/server/ServerFacade.java
@@ -0,0 +1,11 @@
+package server;
+
+public class ServerFacade {
+    private final String serverUrl;
+
+    public ServerFacade(String serverUrl) {
+        this.serverUrl = serverUrl;
+    }
+
+
+}
