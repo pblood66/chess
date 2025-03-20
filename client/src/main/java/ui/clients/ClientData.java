87df80cf991f356e0c3ae4b300e01ffe4ac9f63e
Patrick Blood <pbloo@Patrick-Blood.com>
2025-03-19 23:00:55 -0600

    Added ClientData class to keep track of information

---
+++ client/src/main/java/ui/clients/ClientData.java
@@ -0,0 +1,63 @@
+package ui.clients;
+
+import chess.ChessGame;
+
+public class ClientData {
+    private String authToken;
+    private String username = "";
+    private int gameId;
+    private ChessGame.TeamColor playerColor;
+    private ClientState state;
+
+    public enum ClientState {
+        LOGGED_OUT,
+        LOGGED_IN,
+        IN_GAME
+    }
+
+    public ClientData() {
+        this.authToken = "";
+        this.gameId = 0;
+        this.state = ClientState.LOGGED_OUT;
+    }
+
+    public ClientState getState() {
+        return state;
+    }
+
+    public void setState(ClientState state) {
+        this.state = state;
+    }
+
+    public int getGameId() {
+        return gameId;
+    }
+
+    public void setGameId(int gameId) {
+        this.gameId = gameId;
+    }
+
+    public String getAuthToken() {
+        return authToken;
+    }
+
+    public void setAuthToken(String authToken) {
+        this.authToken = authToken;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public ChessGame.TeamColor getPlayerColor() {
+        return playerColor;
+    }
+
+    public void setPlayerColor(ChessGame.TeamColor playerColor) {
+        this.playerColor = playerColor;
+    }
+}
