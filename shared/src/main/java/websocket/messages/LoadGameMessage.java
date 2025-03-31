a06d8a9f3f99f3b245b10f2df1a3896bcc81174f
Patrick Blood <pbloo@Patrick-Blood.com>
2025-03-31 09:13:21 -0600

    added websocket server messages

---
+++ shared/src/main/java/websocket/messages/LoadGameMessage.java
@@ -0,0 +1,17 @@
+package websocket.messages;
+
+import chess.ChessGame;
+
+public class LoadGameMessage extends ServerMessage {
+
+    private final ChessGame game;
+
+    public LoadGameMessage(ServerMessageType type, ChessGame game) {
+        super(type);
+        this.game = game;
+    }
+
+    public ChessGame getGame() {
+        return game;
+    }
+}
