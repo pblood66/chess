9353e580727b7b63002898fb784d0d9c7dffc78e
Patrick Blood <pbloo@Patrick-Blood.com>
2025-04-06 22:27:32 -0600

    fixed board

--- client/src/main/java/ui/BoardUi.java
+++ client/src/main/java/ui/BoardUi.java
@@ -13,19 +13,27 @@ public class BoardUi {
     private static final String[] COLUMN_HEADERS = {"\u2003a ", "\u2003b ", "\u2003c ", "\u2003d ", "\u2003e ", "\u2003f ",
             "\u2003g ", "\u2003h "};
 
+    public static void main(String[] args) {
+        ChessBoard board = new ChessBoard();
+        board.resetBoard();
+        System.out.println(drawBoard(board, ChessGame.TeamColor.WHITE, null));
+        System.out.println(drawBoard(board, ChessGame.TeamColor.BLACK, null));
+
+    }
+
 
     public static String drawBoard(ChessBoard board, ChessGame.TeamColor playerColor, List<ChessPosition> moves) {
         StringBuilder builder = new StringBuilder();
-        int startRow = (playerColor == ChessGame.TeamColor.WHITE) ? 0 : 7;
-        int endRow = (playerColor == ChessGame.TeamColor.WHITE) ? 8 : -1;
-        int step = (playerColor == ChessGame.TeamColor.WHITE) ? 1 : -1;
+        int startRow = (playerColor == ChessGame.TeamColor.WHITE) ? 7 : 0;
+        int endRow = (playerColor == ChessGame.TeamColor.WHITE) ? -1 : 8;
+        int step = (playerColor == ChessGame.TeamColor.WHITE) ? -1 : 1;
 
         builder.append(SET_TEXT_COLOR_BLUE);
         builder.append(getColumnHeaders(playerColor));
 
         for (int row = startRow; row != endRow; row += step) {
             builder.append(SET_BG_COLOR_DARK_GREY)
-                    .append(" ").append(8 - row).append(" ")
+                    .append(" ").append(row + 1).append(" ")
                     .append(RESET_BG_COLOR);
 
             for (int col = startRow; col != endRow; col += step) {
@@ -34,7 +42,7 @@ public class BoardUi {
             }
 
             builder.append(SET_BG_COLOR_DARK_GREY)
-                    .append(" ").append(8 - row).append(" ")
+                    .append(" ").append(row + 1).append(" ")
                     .append(RESET_BG_COLOR)
                     .append("\n");
         }
